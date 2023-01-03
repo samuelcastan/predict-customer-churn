95bf6c1e3b15d43c7d58dbf52c4bdabbaba5ffbd
Samuel Castan <samuel.castan96@gmail.com>
2023-01-02 22:14:09 -0600

    update file

--- constants.py
+++ constants.py
@@ -9,6 +9,11 @@ CSV_PATH = 'data/bank_data.csv'
 
 RANDOM_STATE = 42
 
+DICT_REPLACE_COL_NAMES = {
+    ' ':'',
+    '-':'_'
+}
+
 CAT_COLUMNS = [
     'Gender',
     'Education_Level',
@@ -34,23 +39,24 @@ QUANT_COLUMNS = [
     'Avg_Utilization_Ratio'
 ]
 
-KEEP_COLS = ['Customer_Age', 
-             'Dependent_count', 
+KEEP_COLS = ['Customer_Age',
+             'Dependent_count',
              'Months_on_book',
-             'Total_Relationship_Count', 
+             'Total_Relationship_Count',
              'Months_Inactive_12_mon',
-             'Contacts_Count_12_mon', 
-             'Credit_Limit', 
+             'Contacts_Count_12_mon',
+             'Credit_Limit',
              'Total_Revolving_Bal',
-             'Avg_Open_To_Buy', 
-             'Total_Amt_Chng_Q4_Q1', 
+             'Avg_Open_To_Buy',
+             'Total_Amt_Chng_Q4_Q1',
              'Total_Trans_Amt',
-             'Total_Trans_Ct', 
-             'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
-             'Gender_Churn', 
-             'Education_Level_Churn', 
+             'Total_Trans_Ct',
+             'Total_Ct_Chng_Q4_Q1', 
+             'Avg_Utilization_Ratio',
+             'Gender_Churn',
+             'Education_Level_Churn',
              'Marital_Status_Churn',
-             'Income_Category_Churn', 
+             'Income_Category_Churn',
              'Card_Category_Churn']
 
 TEST_SIZE = 0.30
